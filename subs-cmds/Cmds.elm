9fc8f86ad5cb32e5e77255d1a719a8068177945d
JiHyun Song <jihyun.song0829@gmail.com>
2017-06-28 23:53:45 +0900

    add comments for basic command script

--- subs-cmds/Cmds.elm
+++ subs-cmds/Cmds.elm
@@ -1,3 +1,4 @@
+-- https://www.elm-tutorial.org/ko/03-subs-cmds/02-commands.html
 module Cmds exposing (..)
 
 import Html exposing (Html, div, button, text, program)
@@ -22,8 +23,8 @@ init =
 
 
 type Msg
-    = Roll
-    | OnResult Int
+    = Roll         -- Roll 는 새로운 수를 요청
+    | OnResult Int -- OnResult 는 Random 라이브러리에서 생성한 수를 돌려받습니다.
 
 
 
@@ -45,7 +46,7 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Roll ->
+        Roll -> -- Random.generate 는 랜덤한 수를 생성하도록 요청하는 커맨드를 만듭니다. 첫 인자는 메시지 생성자인데, 우리 어플리케이션에 돌아오도록 하기 위한 것입니다. 이 경우는 OnResult 입니다.
             ( model, Random.generate OnResult (Random.int 1 6) )
 
         OnResult res ->
